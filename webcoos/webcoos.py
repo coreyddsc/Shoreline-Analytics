7e0bc01f23e19dedb165c66fdfc2b401819474d9
coreyddsc <coreyd.dsc@gmail.com>
2026-03-22 17:25:26 -0400

    added processing stats and inventory filter

--- webcoos/webcoos.py
+++ webcoos/webcoos.py
@@ -29,13 +29,14 @@ class CONST:
         raise ValueError('WebCOOS token is required. Please set the WebCOOS environment variable.')
     else:
         webcoos_token = os.getenv('WebCOOS')
-    image_dir = Path(os.getcwd()) / "images"
+    cwd = Path(os.getcwd())
+    image_dir = cwd / "images"
     station_list = ['oakisland_west', 'jennette_south', 'jennette_north', 'currituck_sailfish', 'currituck_hampton_inn']
     headers = {'Authorization': f'Token {webcoos_token}', 'Accept': 'application/json'}
     endpoint_url = 'https://app.webcoos.org/webcoos/api/v1/elements/'
     
     
-def handle_time(time_str):
+def handle_time(time_str: str):
     """Convert a string to a datetime object."""
     if time_str is None:
         return None
@@ -109,7 +110,7 @@ def check_station_availability(station: str = None):
         
 
 # this should handle multiple service options: stills, videos, annotated, etc.
-def build_inventory(station: str = "oakisland_west", start: str = None, end: str = None, product: str = "one-minute-stills"):
+def build_inventory(station: str, start: str = None, end: str = None, product: str = "one-minute-stills"):
 
     if station is None:
         raise ValueError("Station not set. Please set a station before fetching images.")
@@ -210,12 +211,11 @@ def get_video_metadata(url: str):
 
 
 def process_video(station: str, date: str, url: str, min_duration: float = 450, target_samples: int = 100):
-    img_dir = CONST.image_dir / rf"{station}" / "time_average" / rf"n={target_samples}-sample-frames"
+    
+    img_dir = CONST.image_dir / rf"{station}" / "time_average" / rf"n={target_samples}-frames"
     os.makedirs(img_dir, exist_ok=True)
-    print(f"Date: {date}")
     safe_date = date.replace(':', '')  # Replace colons with hyphens
     formatted_date = safe_date.split('T')[0] + "_" + safe_date.split('T')[1][:4]
-    print(f"Formatted Date: {formatted_date}")
     file_path = img_dir / rf"{station}-{formatted_date}.png"
     
     try:
@@ -225,9 +225,9 @@ def process_video(station: str, date: str, url: str, min_duration: float = 450, 
             print("Error: Could not get frame count or video does not meet minimum duration requirement")
             return None
         
-        target_samples = target_samples
-        sample_interval = max(1, total_frames // target_samples)
-        
+        # Track processing time
+        start = time.time()
+
         cap = cv2.VideoCapture(url)
         if not cap.isOpened():
             print("Error: Could not open downloaded video")
@@ -236,8 +236,9 @@ def process_video(station: str, date: str, url: str, min_duration: float = 450, 
         # Sample frames
         accumulated = None
         sample_count = 0
-        
-        for frame_idx in range(0, total_frames, sample_interval):
+        sample_interval = total_frames / target_samples
+        for i in range(target_samples):
+            frame_idx = int(i * sample_interval)
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
             ret, frame = cap.read()
             
@@ -252,13 +253,24 @@ def process_video(station: str, date: str, url: str, min_duration: float = 450, 
                 sample_count += 1
                 
         cap.release()
+        
+        # Elapsed time for processing
+        elapsed = round(time.time() - start, 3)
 
+        process_stats = {
+            "image_name": rf"{file_path.stem}.png",
+            "date": formatted_date,
+            "video_duration": duration,
+            "video_frames": total_frames,
+            "n_samples": sample_count,
+            "processing_time": elapsed
+        }
+        
         # Create and save average image
         if sample_count > 0:
             average_image = (accumulated / sample_count).astype(np.uint8)
             cv2.imwrite(str(file_path), average_image)
-            print(f"Saved time average image to:\n{file_path}")
-            return sample_count
+            return process_stats
         else:
             print("No frames sampled")
             return None
@@ -268,20 +280,16 @@ def process_video(station: str, date: str, url: str, min_duration: float = 450, 
         return None
 
 
-def process_video_worker(date, url, station, min_duration, target_samples):
+def process_video_worker(date: str, url: str, station: str, min_duration: float, target_samples: int):
     
     # Call your video processing function
     result = process_video(station, date, url, min_duration, target_samples)
+    print(result)
     
-    return {
-        'url': url,
-        'date': date,
-        'frame_count': result,  # or whatever your function returns
-        'station': station
-    }
+    return result
 
 
-def parallel_processor(inventory, station, min_duration, target_samples):
+def parallel_processor(inventory: dict, station: str, min_duration: float, target_samples: int):
     results = []
     with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count() // 2) as executor:
         # Submit each date-url pair
@@ -302,7 +310,29 @@ def parallel_processor(inventory, station, min_duration, target_samples):
             except Exception as e:
                 print(f"Video {date} failed with error: {e}")
     
+    pstats_outf = CONST.cwd / "data" / station / "process_stats.jsonl"
+    pstats_outf.parent.mkdir(parents=True, exist_ok=True)
+    with open(pstats_outf, "w") as f:
+        for result in results:
+            f.write(json.dumps(result) + '\n')
+    
     return results
+
+
+def filter_inventory(inventory: dict, station: str, target_samples: int):
+    "Filters the inventory query to exclude images already processed at a given target sample"
+    img_dir = CONST.image_dir / station / "time_average" / rf"n={target_samples}-frames"
+    img_list = os.listdir(img_dir)
+    img_dates = [img.replace(f'{station}-', '').replace('.png', '') for img in img_list]
+    
+    filt_inv = {}
+    for date, url in inventory.items():
+        safe_date = date.replace(':', '')  # Replace colons with hyphens
+        formatted_date = safe_date.split('T')[0] + "_" + safe_date.split('T')[1][:4]
+        if formatted_date not in img_dates:
+            filt_inv.setdefault(date, url)
+            
+    return filt_inv
 
 
 def main(**kwargs):
@@ -313,31 +343,31 @@ def main(**kwargs):
     product = kwargs.get("product")
     min_duration = kwargs.get("min_vid_duration")
     target_samples = kwargs.get("target_samples")
-    print(f"Number of target samples selected: {target_samples}")
     gtx_flag = kwargs.get("get_time_x")
     parallel_proc = kwargs.get("parallel_proc")
+    dry_run = kwargs.get("dry_run")
+    overwrite = kwargs.get("over_write")
     
     dt_rng = set_date_range(start=start, end=end)
     inventory = build_inventory(station, dt_rng[0], dt_rng[1], product)
-    inv_key_list = list(inventory.keys())
     
-    
-    if product == "video-archive" and not parallel_proc:
-        i = 0
-        for date, url in inventory.items():
-            duration, frames = get_video_metadata(url)
-            print(f"Video Date: {date} | Duration: {duration} | Frame Count: {frames}")
-            if i < 3:
-                print(f"Inventory DATE: {date}")
-                print(f"Inventory URL:\n{url}")
-                sample_count = process_video(station, date, url, min_duration, target_samples)
-                print(f"Sample count used for image: {sample_count} ")
-                if sample_count is not None:
-                    i += 1
-                
-    if product == "video-archive" and parallel_proc:
-        print(f"Parallel Processing with {multiprocessing.cpu_count() // 2} workers")
-        results = parallel_processor(inventory, station, min_duration, target_samples)
+    if not overwrite:
+        inventory = filter_inventory(inventory, station, target_samples)
+        
+    if not dry_run:
+        if product == "video-archive" and not parallel_proc:
+            i = 0
+            for date, url in inventory.items():
+                duration, frames = get_video_metadata(url)
+                print(f"Video Date: {date} | Duration: {duration} | Frame Count: {frames}")
+                if i < 3:
+                    sample_count = process_video(station, date, url, min_duration, target_samples)
+                    if sample_count is not None:
+                        i += 1
+                    
+        if product == "video-archive" and parallel_proc:
+            print(f"Parallel Processing with {multiprocessing.cpu_count() // 2} workers")
+            results = parallel_processor(inventory, station, min_duration, target_samples)
 
 
 
@@ -352,6 +382,8 @@ def cli():
     parser.add_argument('-t', '--target-samples', type=int, help="Number of sample frames to use in video to image processing", default=100)
     parser.add_argument('-gtx', "--get-time-x", action="store_true", help="Apply Static getTimexShoreline Extract Methods") # store default as false to not use static extract
     parser.add_argument('-ll', '--parallel-proc', action="store_true", help="Use Parallel Processing") # store default as true for parallel processing
+    parser.add_argument('-dry-run', action="store_true", help="Dry run will not process videos to images")
+    parser.add_argument('-o', '--over-write', action="store_true", help="Overwrite will not filter the inventory list for existing time average images")
     args = parser.parse_args()
     main(**vars(args))
 
